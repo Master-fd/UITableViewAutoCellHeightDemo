4b0a51d001b4fb2c73cb3932101bab34e4d9620c
Master-fd <asus@asusdeMac-Pro.local>
2016-07-23 23:33:49 +0800

    Initial Commit

---
+++ UITableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewDemo
+//
+//  Created by asus on 16/7/23.
+//  Copyright (c) 2016年 asus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
